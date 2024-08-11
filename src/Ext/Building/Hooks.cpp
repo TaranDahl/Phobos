0c4be5248459b19bb0fa5049a72969fe2167e933
CrimRecya <335958461@qq.com>
2024-08-11 17:15:42 +0800

    Fix

--- src/Ext/Building/Hooks.cpp
+++ src/Ext/Building/Hooks.cpp
@@ -324,8 +324,8 @@ DEFINE_HOOK(0x443CCA, BuildingClass_KickOutUnit_AircraftType_Phobos, 0xA)
 	return 0;
 }
 
-//
-DEFINE_HOOK(0x450292, BuildingClass_UpdateFactory_KickOutStuckUnits, 0x6)
+// Kick out stuck units when the factory building is not busy
+DEFINE_HOOK(0x450248, BuildingClass_UpdateFactory_KickOutStuckUnits, 0x6)
 {
 	GET(BuildingClass*, pThis, ESI);
 
@@ -335,7 +335,7 @@ DEFINE_HOOK(0x450292, BuildingClass_UpdateFactory_KickOutStuckUnits, 0x6)
 	return 0;
 }
 
-//
+// Should not kick out units if the factory building is in construction process
 DEFINE_HOOK(0x4444A0, BuildingClass_KickOutUnit_NoKickOutInConstruction, 0x7)
 {
 	enum { OfCourse = 0x444565, NoChance = 0x4444B3};
