30aff86b63d1737e3a4fab65c3b9d05d9f7549b2
CrimRecya <335958461@qq.com>
2024-09-02 22:45:04 +0800

    Delete TracingTrajectory useless

--- src/Ext/Bullet/Trajectories/TracingTrajectory.cpp
+++ src/Ext/Bullet/Trajectories/TracingTrajectory.cpp
@@ -7,17 +7,6 @@ bool TracingTrajectoryType::Load(PhobosStreamReader& Stm, bool RegisterForChange
 
 	Stm
 		.Process(this->TheDuration, false)
-		.Process(this->RelockDelay, false)
-		.Process(this->RelockRange, false)
-		.Process(this->NoRelockROF, false)
-		.Process(this->AddedRange, false)
-		.Process(this->IsLaser, false)
-		.Process(this->TargetLaser, false)
-		.Process(this->FinishLaser, false)
-		.Process(this->IsHouseColor, false)
-		.Process(this->LaserColor, false)
-		.Process(this->BlazeDelay, false)
-		.Process(this->DamageDelay, false)
 		;
 
 	return true;
@@ -29,17 +18,6 @@ bool TracingTrajectoryType::Save(PhobosStreamWriter& Stm) const
 
 	Stm
 		.Process(this->TheDuration)
-		.Process(this->RelockDelay)
-		.Process(this->RelockRange)
-		.Process(this->NoRelockROF)
-		.Process(this->AddedRange)
-		.Process(this->IsLaser)
-		.Process(this->TargetLaser)
-		.Process(this->FinishLaser)
-		.Process(this->IsHouseColor)
-		.Process(this->LaserColor)
-		.Process(this->BlazeDelay)
-		.Process(this->DamageDelay)
 		;
 
 	return true;
@@ -54,17 +32,6 @@ void TracingTrajectoryType::Read(CCINIClass* const pINI, const char* pSection)
 {
 	INI_EX exINI(pINI);
 	this->TheDuration.Read(exINI, pSection, "Trajectory.Tracing.TheDuration");
-	this->RelockDelay.Read(exINI, pSection, "Trajectory.Tracing.RelockDelay");
-	this->RelockRange.Read(exINI, pSection, "Trajectory.Tracing.RelockRange");
-	this->NoRelockROF.Read(exINI, pSection, "Trajectory.Tracing.NoRelockROF");
-	this->AddedRange.Read(exINI, pSection, "Trajectory.Tracing.AddedRange");
-	this->IsLaser.Read(exINI, pSection, "Trajectory.Tracing.IsLaser");
-	this->TargetLaser.Read(exINI, pSection, "Trajectory.Tracing.TargetLaser");
-	this->FinishLaser.Read(exINI, pSection, "Trajectory.Tracing.FinishLaser");
-	this->IsHouseColor.Read(exINI, pSection, "Trajectory.Tracing.IsHouseColor");
-	this->LaserColor.Read(exINI, pSection, "Trajectory.Tracing.LaserColor");
-	this->BlazeDelay.Read(exINI, pSection, "Trajectory.Tracing.BlazeDelay");
-	this->DamageDelay.Read(exINI, pSection, "Trajectory.Tracing.DamageDelay");
 }
 
 bool TracingTrajectory::Load(PhobosStreamReader& Stm, bool RegisterForChange)
@@ -72,20 +39,6 @@ bool TracingTrajectory::Load(PhobosStreamReader& Stm, bool RegisterForChange)
 	this->PhobosTrajectory::Load(Stm, false);
 
 	Stm
-		.Process(this->RelockDelay)
-		.Process(this->RelockRange)
-		.Process(this->NoRelockROF)
-		.Process(this->AddedRange)
-		.Process(this->IsLaser)
-		.Process(this->TargetLaser)
-		.Process(this->FinishLaser)
-		.Process(this->IsHouseColor)
-		.Process(this->LaserColor)
-		.Process(this->BlazeDelay)
-		.Process(this->DamageDelay)
-		.Process(this->RelockTimer)
-		.Process(this->BlazeTimer)
-		.Process(this->DamageTimer)
 		.Process(this->ExistTimer)
 		;
 
@@ -97,20 +50,6 @@ bool TracingTrajectory::Save(PhobosStreamWriter& Stm) const
 	this->PhobosTrajectory::Save(Stm);
 
 	Stm
-		.Process(this->RelockDelay)
-		.Process(this->RelockRange)
-		.Process(this->NoRelockROF)
-		.Process(this->AddedRange)
-		.Process(this->IsLaser)
-		.Process(this->TargetLaser)
-		.Process(this->FinishLaser)
-		.Process(this->IsHouseColor)
-		.Process(this->LaserColor)
-		.Process(this->BlazeDelay)
-		.Process(this->DamageDelay)
-		.Process(this->RelockTimer)
-		.Process(this->BlazeTimer)
-		.Process(this->DamageTimer)
 		.Process(this->ExistTimer)
 		;
 
@@ -122,17 +61,6 @@ void TracingTrajectory::OnUnlimbo(BulletClass* pBullet, CoordStruct* pCoord, Bul
 	auto const pType = this->GetTrajectoryType<TracingTrajectoryType>(pBullet);
 
 	int theDuration = pType->TheDuration;
-	this->RelockDelay = pType->RelockDelay;
-	this->RelockRange = pType->RelockRange;
-	this->NoRelockROF = pType->NoRelockROF;
-	this->AddedRange = pType->AddedRange;
-	this->IsLaser = pType->IsLaser;
-	this->TargetLaser = pType->TargetLaser;
-	this->FinishLaser = pType->FinishLaser;
-	this->IsHouseColor = pType->IsHouseColor;
-	this->LaserColor = pType->LaserColor;
-	this->BlazeDelay = pType->BlazeDelay;
-	this->DamageDelay = pType->DamageDelay;
 
 	if (theDuration <= 0)
 	{
--- src/Ext/Bullet/Trajectories/TracingTrajectory.h
+++ src/Ext/Bullet/Trajectories/TracingTrajectory.h
@@ -7,17 +7,6 @@ class TracingTrajectoryType final : public PhobosTrajectoryType
 public:
 	TracingTrajectoryType() : PhobosTrajectoryType(TrajectoryFlag::Tracing)
 		, TheDuration { 0 }
-		, RelockDelay { 20 }
-		, RelockRange { 5.0 }
-		, AddedRange { 5.0 }
-		, NoRelockROF { 0 }
-		, IsLaser { true }
-		, TargetLaser { true }
-		, FinishLaser { true }
-		, IsHouseColor { false }
-		, LaserColor { { 0, 0, 0 } }
-		, BlazeDelay { 20 }
-		, DamageDelay { 5 }
 	{}
 
 	virtual bool Load(PhobosStreamReader& Stm, bool RegisterForChange) override;
@@ -26,55 +15,16 @@ public:
 	virtual void Read(CCINIClass* const pINI, const char* pSection) override;
 
 	Valueable<int> TheDuration;
-	Valueable<int> RelockDelay;
-	Valueable<double> RelockRange;
-	Valueable<double> AddedRange;
-	Valueable<int> NoRelockROF;
-	Valueable<bool> IsLaser;
-	Valueable<bool> TargetLaser;
-	Valueable<bool> FinishLaser;
-	Valueable<bool> IsHouseColor;
-	Valueable<ColorStruct> LaserColor;
-	Valueable<int> BlazeDelay;
-	Valueable<int> DamageDelay;
 };
 
 class TracingTrajectory final : public PhobosTrajectory
 {
 public:
 	TracingTrajectory() : PhobosTrajectory(TrajectoryFlag::Tracing)
-		, RelockDelay { 20 }
-		, RelockRange { 5.0 }
-		, AddedRange { 5.0 }
-		, NoRelockROF { 0 }
-		, IsLaser { true }
-		, TargetLaser { true }
-		, FinishLaser { true }
-		, IsHouseColor { false }
-		, LaserColor {}
-		, BlazeDelay { 20 }
-		, DamageDelay { 5 }
-		, RelockTimer {}
-		, BlazeTimer {}
-		, DamageTimer {}
 		, ExistTimer {}
 	{}
 
 	TracingTrajectory(PhobosTrajectoryType const* pType) : PhobosTrajectory(TrajectoryFlag::Tracing)
-		, RelockDelay { 20 }
-		, RelockRange { 5.0 }
-		, AddedRange { 5.0 }
-		, NoRelockROF { 0 }
-		, IsLaser { true }
-		, TargetLaser { true }
-		, FinishLaser { true }
-		, IsHouseColor { false }
-		, LaserColor {}
-		, BlazeDelay { 20 }
-		, DamageDelay { 5 }
-		, RelockTimer {}
-		, BlazeTimer {}
-		, DamageTimer {}
 		, ExistTimer {}
 	{}
 
@@ -88,20 +38,6 @@ public:
 	virtual TrajectoryCheckReturnType OnAITargetCoordCheck(BulletClass* pBullet) override;
 	virtual TrajectoryCheckReturnType OnAITechnoCheck(BulletClass* pBullet, TechnoClass* pTechno) override;
 
-	int RelockDelay;
-	double RelockRange;
-	double AddedRange;
-	int NoRelockROF;
-	bool IsLaser;
-	bool TargetLaser;
-	bool FinishLaser;
-	bool IsHouseColor;
-	ColorStruct LaserColor;
-	int BlazeDelay;
-	int DamageDelay;
-	CDTimerClass RelockTimer;
-	CDTimerClass BlazeTimer;
-	CDTimerClass DamageTimer;
 	CDTimerClass ExistTimer;
 
 private:
