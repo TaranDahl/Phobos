811f594228bc1f9bcad1a0eecc3b51cb3af15fed
CrimRecya <335958461@qq.com>
2024-10-08 22:21:12 +0800

    Update AI base center

--- src/Ext/Techno/Hooks.Misc.cpp
+++ src/Ext/Techno/Hooks.Misc.cpp
@@ -585,4 +585,47 @@ DEFINE_HOOK(0x739889, UnitClass_TryToDeploy_AISetBaseCenter, 0x6)
 	return (RulesExt::Global()->AISetBaseCenter && pMCV->Owner->NumConYards > 1) ? SkipGameCode : 0;
 }
 
+DEFINE_HOOK(0x4FD538, HouseClass_AIHouseUpdate_CheckAIBaseCenter, 0x7)
+{
+	GET(HouseClass*, pAI, EBX);
+
+	if (const int count = pAI->ConYards.Count)
+	{
+		const int wayPoint = pAI->GetSpawnPosition();
+
+		if (wayPoint != -1)
+		{
+			const CellStruct center = ScenarioClass::Instance->GetWaypointCoords(wayPoint);
+			CellStruct newCenter = center;
+			double distanceSquared = 131072.0;
+
+			for (int i = 0; i < count; ++i)
+			{
+				if (BuildingClass* const pBuilding = pAI->ConYards.GetItem(i))
+				{
+					if (pBuilding->IsAlive && pBuilding->Health && !pBuilding->InLimbo)
+					{
+						const double newDistanceSquared = pBuilding->GetMapCoords().DistanceFromSquared(center);
+
+						if (newDistanceSquared > 128.0 && newDistanceSquared < distanceSquared)
+						{
+							distanceSquared = newDistanceSquared;
+							newCenter = pBuilding->GetMapCoords();
+						}
+					}
+				}
+			}
+
+			if (newCenter != center)
+			{
+				pAI->BaseSpawnCell = newCenter;
+				pAI->Base.BaseNodes.Items->MapCoords = newCenter;
+				pAI->Base.Center = newCenter;
+			}
+		}
+	}
+
+	return 0;
+}
+
 #pragma endregion
