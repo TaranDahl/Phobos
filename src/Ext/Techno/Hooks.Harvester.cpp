7e6f220f9cb586c17c12e07c28f0bbc0fc01d8b0
TaranDahl <sd_zealot@163.com>
2024-10-11 23:37:08 +0800

    proto, unfinished

--- src/Ext/Techno/Hooks.Harvester.cpp
+++ src/Ext/Techno/Hooks.Harvester.cpp
@@ -19,8 +19,8 @@ DEFINE_HOOK(0x73E730, UnitClass_MissionHarvest_HarvesterScanAfterUnload, 0x5)
 	// Focus is set when the harvester is fully loaded and go home.
 	if (pFocus)
 	{
-		CellStruct* pCellBuffer;
-		CellStruct* pCellStru = (CellStruct*)pThis->ScanForTiberium((DWORD)pCellBuffer, RulesClass::Instance->TiberiumLongScan / 256, 0);
+		CellStruct cellBuffer = { 0,0 };
+		CellStruct* pCellStru = (CellStruct*)pThis->ScanForTiberium((DWORD)&cellBuffer, RulesClass::Instance->TiberiumLongScan / 256, 0);
 
 		if (*pCellStru != CellStruct::Empty)
 		{
@@ -42,11 +42,63 @@ DEFINE_HOOK(0x73E730, UnitClass_MissionHarvest_HarvesterScanAfterUnload, 0x5)
 #pragma region HarvesterQuickUnload
 
 /*
-DEFINE_HOOK(0x, UnitClass_MissionHarvest_GoHome, 0x)
+// MissionStatus == 2 means the unit is returning.
+DEFINE_HOOK(0x, UnitClass_MissionHarvest_Status2, 0x)
 {
 
 }
+
 */
+
+DEFINE_HOOK(0x73E755, TEST, 0x6)
+{
+	GET(UnitClass*, pThis, EBP);
+
+	if (!pThis->Destination)
+		return 0;
+
+	auto ParasiteEatingMe = pThis->ParasiteEatingMe;
+	if (ParasiteEatingMe)
+		ParasiteEatingMe->ParasiteImUsing->ExitUnit();
+	pThis->Mark(MarkType::Up);
+	auto v28 = pThis;
+	auto ChronoOutSound = v28->GetTechnoType()->ChronoOutSound;
+	CoordStruct pCoord;
+	auto LastCoords = pThis->Destination->GetCoords();
+	if (ChronoOutSound != -1 || (ChronoOutSound = RulesClass::Instance->ChronoOutSound, ChronoOutSound != -1))
+	{
+		pCoord = v28->Location;
+		VocClass::PlayAt(ChronoOutSound, pCoord, 0);
+	}
+	CellStruct cell = { LastCoords.X / 256, LastCoords.Y / 256 };
+	auto v46 = cell;
+	auto CellAt = MapClass::Instance->GetCellAt(v46);
+	pThis->SetLocation(LastCoords);
+	pThis->OnBridge = 0;//(CellAt->Flags & 0x100) != 0;
+	pThis->SetHeight(0);
+	pThis->Mark(MarkType::Down);
+	auto pUnit = pThis;
+	auto ChronoInSound = pUnit->GetTechnoType()->ChronoInSound;
+	if (ChronoInSound != -1 || (ChronoInSound = RulesClass::Instance->ChronoInSound, ChronoInSound != -1))
+	{
+		pCoord = pUnit->Location;
+		VocClass::PlayAt(ChronoInSound, pCoord, 0);
+	}
+	pThis->UpdatePosition(PCPType::End);
+	pThis->SetDestination(0, 1);
+	/*
+	(pThis->LocomotionClassOffset::IPersistStream::IPersist::IUnknown::__vftable[1].Save)(pThis);
+	CellClass::CollectCrate(CellAt, pThis);
+	v33 = operator new(0x1C8u);
+	if (v33)
+	{
+		pCoord = pThis->Location;
+		AnimClass::CTOR(v33, RulesClass::Instance->WarpOut, &pCoord, 0, 1, 1536, 0, 0);
+	}*/
+	pThis->unknown_280 = 0;
+
+	return 0;
+}
 
 #pragma endregion
 
