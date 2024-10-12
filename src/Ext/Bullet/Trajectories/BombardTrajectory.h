8767e8528dacd9c66cf3fcd3a369feb6fe59b979
CrimRecya <335958461@qq.com>
2024-10-12 22:04:30 +0800

    Line trail fix

--- src/Ext/Bullet/Trajectories/BombardTrajectory.h
+++ src/Ext/Bullet/Trajectories/BombardTrajectory.h
@@ -117,5 +117,6 @@ private:
 	bool BulletDetonatePreCheck(BulletClass* pBullet);
 	bool BulletDetonateRemainCheck(BulletClass* pBullet, HouseClass* pOwner);
 	void BulletVelocityChange(BulletClass* pBullet);
+	void RefreshBulletLineTrail(BulletClass* pBullet);
 	void ApplyTurningPointAnim(const std::vector<AnimTypeClass*>& AnimList, CoordStruct coords, TechnoClass* pTechno = nullptr, HouseClass* pHouse = nullptr, bool invoker = false, bool ownedObject = false);;
 };
