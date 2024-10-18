1c6053dc669450fc357525b605b2ee8176129627
CrimRecya <335958461@qq.com>
2024-10-18 17:40:31 +0800

    Fix merge

---
+++ src/Ext/Techno/Hooks.Others.cpp
@@ -0,0 +1,171 @@
+#include "Body.h"
+
+#include <Ext/WeaponType/Body.h>
+
+#pragma region NoBurstDelay
+
+DEFINE_HOOK(0x5209EE, InfantryClass_UpdateFiring_BurstNoDelay, 0x5)
+{
+	enum { SkipVanillaFire = 0x520A57 };
+
+	GET(InfantryClass* const, pThis, EBP);
+	GET(const int, wpIdx, ESI);
+	GET(AbstractClass* const, pTarget, EAX);
+
+	if (const WeaponTypeClass* const pWeapon = pThis->GetWeapon(wpIdx)->WeaponType)
+	{
+		if (pWeapon->Burst > 1)
+		{
+			if (const WeaponTypeExt::ExtData* const pWeaponExt = WeaponTypeExt::ExtMap.Find(pWeapon))
+			{
+				if (pWeaponExt->Burst_NoDelay)
+				{
+					if (pThis->Fire(pTarget, wpIdx))
+					{
+						int rof = pThis->RearmTimer.TimeLeft;
+						pThis->RearmTimer.Start(0);
+
+						for (int i = 1; i != pWeapon->Burst && pThis->GetFireError(pTarget, wpIdx, true) == FireError::OK && pThis->Fire(pTarget, wpIdx); ++i)
+						{
+							rof = pThis->RearmTimer.TimeLeft;
+							pThis->RearmTimer.Start(0);
+						}
+
+						pThis->RearmTimer.Start(rof);
+					}
+
+					return SkipVanillaFire;
+				}
+			}
+		}
+	}
+
+	return 0;
+}
+
+DEFINE_HOOK(0x736F67, UnitClass_UpdateFiring_BurstNoDelay, 0x6)
+{
+	enum { SkipVanillaFire = 0x737063 };
+
+	GET(UnitClass* const, pThis, ESI);
+	GET(const int, wpIdx, EDI);
+	GET(AbstractClass* const, pTarget, EAX);
+
+	if (const WeaponTypeClass* const pWeapon = pThis->GetWeapon(wpIdx)->WeaponType)
+	{
+		if (pWeapon->Burst > 1)
+		{
+			if (const WeaponTypeExt::ExtData* const pWeaponExt = WeaponTypeExt::ExtMap.Find(pWeapon))
+			{
+				if (pWeaponExt->Burst_NoDelay)
+				{
+					if (pThis->Fire(pTarget, wpIdx))
+					{
+						int rof = pThis->RearmTimer.TimeLeft;
+						pThis->RearmTimer.Start(0);
+
+						for (int i = 1; i != pWeapon->Burst && pThis->GetFireError(pTarget, wpIdx, true) == FireError::OK && pThis->Fire(pTarget, wpIdx); ++i)
+						{
+							rof = pThis->RearmTimer.TimeLeft;
+							pThis->RearmTimer.Start(0);
+						}
+
+						pThis->RearmTimer.Start(rof);
+					}
+
+					return SkipVanillaFire;
+				}
+			}
+		}
+	}
+
+	return 0;
+}
+
+#pragma endregion
+
+#pragma region CheckFacingWhenRearming
+
+DEFINE_HOOK(0x7410BB, UnitClass_GetFireError_CheckFacingError, 0x8)
+{
+	enum { NoNeedToCheck = 0x74132B, ContinueCheck = 0x7410C3 };
+
+	GET(FireError, fireError, EAX);
+
+	if (fireError == FireError::OK)
+		return ContinueCheck;
+
+	GET(UnitClass*, pThis, ESI);
+
+	return (fireError == FireError::REARM && !pThis->Type->Turret) ? ContinueCheck : NoNeedToCheck;
+}
+
+#pragma endregion
+
+#pragma region AIConstructionYard
+
+DEFINE_HOOK(0x740A11, UnitClass_Mission_Guard_AIAutoDeployMCV, 0x6)
+{
+	enum { SkipGameCode = 0x740A50 };
+
+	GET(UnitClass*, pMCV, ESI);
+
+	return (RulesExt::Global()->AIAutoDeployMCV && pMCV->Owner->NumConYards > 0) ? SkipGameCode : 0;
+}
+
+DEFINE_HOOK(0x739889, UnitClass_TryToDeploy_AISetBaseCenter, 0x6)
+{
+	enum { SkipGameCode = 0x73992B };
+
+	GET(UnitClass*, pMCV, EBP);
+
+	return (RulesExt::Global()->AISetBaseCenter && pMCV->Owner->NumConYards > 1) ? SkipGameCode : 0;
+}
+
+DEFINE_HOOK(0x4FD538, HouseClass_AIHouseUpdate_CheckAIBaseCenter, 0x7)
+{
+	if (RulesExt::Global()->AIBiasSpawnCell && SessionClass::Instance->GameMode != GameMode::Campaign)
+	{
+		GET(HouseClass*, pAI, EBX);
+
+		if (const int count = pAI->ConYards.Count)
+		{
+			const int wayPoint = pAI->GetSpawnPosition();
+
+			if (wayPoint != -1)
+			{
+				const CellStruct center = ScenarioClass::Instance->GetWaypointCoords(wayPoint);
+				CellStruct newCenter = center;
+				double distanceSquared = 131072.0;
+
+				for (int i = 0; i < count; ++i)
+				{
+					if (BuildingClass* const pBuilding = pAI->ConYards.GetItem(i))
+					{
+						if (pBuilding->IsAlive && pBuilding->Health && !pBuilding->InLimbo)
+						{
+							const double newDistanceSquared = pBuilding->GetMapCoords().DistanceFromSquared(center);
+
+							if (newDistanceSquared < distanceSquared)
+							{
+								distanceSquared = newDistanceSquared;
+								newCenter = pBuilding->GetMapCoords();
+							}
+						}
+					}
+				}
+
+				if (newCenter != center)
+				{
+					pAI->BaseSpawnCell = newCenter;
+					pAI->Base.BaseNodes.Items->MapCoords = newCenter;
+					pAI->Base.Center = newCenter;
+				}
+			}
+		}
+	}
+
+	return 0;
+}
+
+#pragma endregion
