a5a617e265ce89cfdbadbafeda35df9ad75b5ade
CrimRecya <335958461@qq.com>
2024-10-27 18:15:04 +0800

    Rename

--- src/Ext/Rules/Body.h
+++ src/Ext/Rules/Body.h
@@ -138,7 +138,7 @@ public:
 		PhobosPCXFile SelectedVehicleMissingPCX;
 		PhobosPCXFile SelectedAircraftMissingPCX;
 		PhobosPCXFile SelectedBuildingMissingPCX;
-		Valueable<bool> AIAutoDeployMCV;
+		Valueable<bool> AINonAutoDeploy;
 		Valueable<bool> AISetBaseCenter;
 		Valueable<bool> AIBiasSpawnCell;
 		Valueable<bool> AIForbidConYard;
@@ -364,7 +364,7 @@ public:
 			, ExpandLandGridFrames { { 1, 0, 0 } }
 			, ExpandWaterGridFrames { { 1, 0, 0 } }
 			, SWSidebarBackground { true }
-			, AIAutoDeployMCV { false }
+			, AINonAutoDeploy { false }
 			, AISetBaseCenter { false }
 			, AIBiasSpawnCell { false }
 			, AIForbidConYard { false }
