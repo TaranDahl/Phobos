48b8f9609f93904ee29cf1fd3e8dd9f24f96e2e6
CrimRecya <335958461@qq.com>
2024-12-26 13:48:52 +0800

    移除合并后的重复钩子

--- src/Ext/Techno/Hooks.Others.cpp
+++ src/Ext/Techno/Hooks.Others.cpp
@@ -826,67 +826,6 @@ DEFINE_HOOK(0x44733A, BuildingClass_MouseOverObject_BuildingCheckDeploy, 0xA)
 
 #pragma region AggressiveAttackMove
 
-DEFINE_HOOK(0x4C75DA, EventClass_RespondToEvent_Stop, 0x6)
-{
-	enum { SkipGameCode = 0x4C762A };
-
-	GET(TechnoClass* const, pTechno, ESI);
-
-	// Check aircrafts
-	const auto pAircraft = abstract_cast<AircraftClass*>(pTechno);
-	const bool commonAircraft = pAircraft && !pAircraft->Airstrike && !pAircraft->Spawned;
-	const auto mission = pTechno->CurrentMission;
-
-	// To avoid aircrafts overlap by keep link if is returning or is in airport now.
-	if (!commonAircraft || (mission != Mission::Sleep && mission != Mission::Guard && mission != Mission::Enter) || !pAircraft->DockNowHeadingTo || (pAircraft->DockNowHeadingTo != pAircraft->GetNthLink()))
-		pTechno->SendToEachLink(RadioCommand::NotifyUnlink);
-
-	// To avoid technos being unable to stop in attack move mega mission
-	if (pTechno->vt_entry_4C4()) // pTechno->MegaMissionIsAttackMove()
-		pTechno->vt_entry_4A8(); // pTechno->ClearMegaMissionData()
-
-	// Clearing the current target should still be necessary for all technos
-	pTechno->SetTarget(nullptr);
-
-	if (commonAircraft)
-	{
-		if (pAircraft->Type->AirportBound)
-		{
-			// To avoid `AirportBound=yes` aircrafts pausing in the air and let they returning to air base immediately.
-			if (!pAircraft->DockNowHeadingTo || (pAircraft->DockNowHeadingTo != pAircraft->GetNthLink())) // If the aircraft have no valid dock, try to find a new one
-				pAircraft->EnterIdleMode(false, true);
-		}
-		else if (pAircraft->Ammo)
-		{
-			// To avoid `AirportBound=no` aircrafts ignoring the stop task or directly return to the airport.
-			if (pAircraft->Destination && static_cast<int>(CellClass::Coord2Cell(pAircraft->Destination->GetCoords()).DistanceFromSquared(pAircraft->GetMapCoords())) > 2) // If the aircraft is moving, find the forward cell then stop in it
-				pAircraft->SetDestination(pAircraft->GetCell()->GetNeighbourCell(static_cast<FacingType>(pAircraft->PrimaryFacing.Current().GetValue<3>())), true);
-		}
-		else if (!pAircraft->DockNowHeadingTo || (pAircraft->DockNowHeadingTo != pAircraft->GetNthLink()))
-		{
-			pAircraft->EnterIdleMode(false, true);
-		}
-		// Otherwise landing or idling normally without answering the stop command
-	}
-	else
-	{
-		// Check Jumpjets
-		const auto pFoot = abstract_cast<FootClass*>(pTechno);
-		const auto pJumpjetLoco = pFoot ? locomotion_cast<JumpjetLocomotionClass*>(pFoot->Locomotor) : nullptr;
-
-		// To avoid jumpjets falling into a state of standing idly by
-		if (!pJumpjetLoco) // If is not jumpjet, clear the destination is enough
-			pTechno->SetDestination(nullptr, true);
-		else if (!pFoot->Destination) // When in attack move and have had a target, the destination will be cleaned up, enter the guard mission can prevent the jumpjets stuck in a status of standing idly by
-			pTechno->QueueMission(Mission::Guard, true);
-		else if (static_cast<int>(CellClass::Coord2Cell(pFoot->Destination->GetCoords()).DistanceFromSquared(pTechno->GetMapCoords())) > 2) // If the jumpjet is moving, find the forward cell then stop in it
-			pTechno->SetDestination(pTechno->GetCell()->GetNeighbourCell(static_cast<FacingType>(pJumpjetLoco->LocomotionFacing.Current().GetValue<3>())), true);
-		// Otherwise landing or idling normally without answering the stop command
-	}
-
-	return SkipGameCode;
-}
-
 static inline bool CheckAttackMoveCanResetTarget(FootClass* pThis)
 {
 	const auto pTarget = pThis->Target;
