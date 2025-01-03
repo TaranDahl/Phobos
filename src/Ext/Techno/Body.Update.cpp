11f1a5624dec4b78b9970cd513b4442741e0fa97
CrimRecya <335958461@qq.com>
2025-01-03 13:26:35 +0800

    换一种方法延长计时

--- src/Ext/Techno/Body.Update.cpp
+++ src/Ext/Techno/Body.Update.cpp
@@ -623,10 +623,10 @@ void TechnoExt::ExtData::UpdateRearmInEMPState()
 	if (pThis->IsUnderEMP() && this->TypeExtData->NoRearmInEMPState.Get(RulesExt::Global()->NoRearmInEMPState))
 	{
 		if (pThis->RearmTimer.InProgress())
-			pThis->RearmTimer.TimeLeft++;
+			pThis->RearmTimer.StartTime++;
 
 		if (pThis->ReloadTimer.InProgress())
-			pThis->ReloadTimer.TimeLeft++;
+			pThis->ReloadTimer.StartTime++;
 	}
 }
 
@@ -637,10 +637,10 @@ void TechnoExt::ExtData::UpdateRearmInTemporal()
 	if (this->TypeExtData->NoRearmInTemporal.Get(RulesExt::Global()->NoRearmInTemporal))
 	{
 		if (pThis->RearmTimer.InProgress())
-			pThis->RearmTimer.TimeLeft++;
+			pThis->RearmTimer.StartTime++;
 
 		if (pThis->ReloadTimer.InProgress())
-			pThis->ReloadTimer.TimeLeft++;
+			pThis->ReloadTimer.StartTime++;
 	}
 }
 
