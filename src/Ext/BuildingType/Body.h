9d87b8deacf6c628183ac6cab3142d45c1828c2e
CrimRecya <335958461@qq.com>
2025-02-08 15:27:02 +0800

    新的单位拓展建筑建造范围的标签

--- src/Ext/BuildingType/Body.h
+++ src/Ext/BuildingType/Body.h
@@ -89,6 +89,8 @@ public:
 		Valueable<bool> NoBuildAreaOnBuildup;
 		ValueableVector<BuildingTypeClass*> Adjacent_Allowed;
 		ValueableVector<BuildingTypeClass*> Adjacent_Disallowed;
+		ValueableVector<UnitTypeClass*> Adjacent_AllowedUnit;
+		ValueableVector<UnitTypeClass*> Adjacent_DisallowedUnit;
 
 		Nullable<Point2D> BarracksExitCell;
 
@@ -149,6 +151,8 @@ public:
 			, NoBuildAreaOnBuildup { false }
 			, Adjacent_Allowed {}
 			, Adjacent_Disallowed {}
+			, Adjacent_AllowedUnit {}
+			, Adjacent_DisallowedUnit {}
 			, BarracksExitCell {}
 		{ }
 
